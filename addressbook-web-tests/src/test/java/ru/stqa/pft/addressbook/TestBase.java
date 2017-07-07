b93826688690dbf425ed0254da25afdd9b4a6921
QL-Anton <antohakiselev@yandex.ru>
2017-07-07 22:18:45 +0500

    Применён механизм делегирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -1,96 +1,31 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.util.concurrent.TimeUnit;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 /**
  * Created by Антон on 02.07.2017.
  */
-public class TestBase {
+public class TestBase  {
 
-  FirefoxDriver wd;
-
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+  protected final ApplicationManager app = new ApplicationManager();
 
   @BeforeMethod
   public void setUp() throws Exception {
-    WebDriver driver;
-    System.setProperty("webdriver.gecko.driver", "D:\\geckodriver\\geckodriver.exe");
-    driver = new FirefoxDriver();
 
 
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
-    wd.get("http://localhost/addressbook/group.php");
-    login("admin", "secret");
+    app.init();
 
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("a");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
-
-  protected void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  protected void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  protected void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  protected void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
-  }
-
-  protected void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod
   public void tearDown() {
-    wd.quit();
+    app.stop();
   }
 
-  protected void deleteSelectedGroups() {
-      wd.findElement(By.name("delete")).click();
-  }
-
-  protected void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
+  public ApplicationManager getApp() {
+    return app;
   }
 }
