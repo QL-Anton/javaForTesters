a899a4096532478751187b457a206af3213eecad
QL-Anton <antohakiselev@yandex.ru>
2017-07-11 00:02:06 +0500

    Добавлен новый тест для модификации контактов.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -17,31 +17,35 @@ public class ContactHelper extends  HelperBase {
 
   }
 
-  public void fillContactForm(ContactData contactData){
+  public void fillContactForm(ContactData contactData) {
     type(By.name("firstname"), contactData.getFirst_name());
     type(By.name("lastname"), contactData.getLast_name());
     type(By.name("address"), contactData.getMobile_phone());
     type(By.name("mobile"), contactData.getMobile_phone());
-    type(By.name("email"),contactData.getE_mail());
+    type(By.name("email"), contactData.getE_mail());
 
   }
-public  void submitContactCreation(){
+
+  public void submitContactCreation() {
     click(By.xpath("//div[@id='content']/form/input[21]"));
+  }
+
+
+  public void setContact() {
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void goToMainPage() {
+    click(By.id("logo"));
+  }
+
+  public void deleteSelectedContact() {
+    click(By.xpath("//div[@id='content']/form[2]/input[2]"));
+  }
+
+
+  public void submitModificationContact() {
+    click(By.xpath("//div[@id='content']/form[1]/input[22]"));
+  }
+
 }
-
-
-
-public void setContact(){
-  click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
-}
-
-public void goToMainPage(){
-  click(By.id("logo"));
-}
-
-public void deleteSelectedContact(){
-  click(By.xpath("//div[@id='content']/form[2]/input[2]"));
-}
-
-}
-
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import static org.openqa.selenium.OutputType.*;
+
+public class ContactModificationTests extends TestBase {
+
+    
+    @Test
+    public void testContactModification() {
+
+      app.getContactHelper().goToMainPage();
+      app.getContactHelper().setContact();
+      app.getContactHelper().fillContactForm(new ContactData("testfn_mod", "testln_mod", "testad_mod", "91799992", "test@test.test"));
+      app.getContactHelper().submitModificationContact();
+      app.getContactHelper().goToMainPage();
+    }
+    
+
+    
+
+
+
+}
