43fc9c85c8bb989a6077400a3bdd0fa3738c8dc1
QL-Anton <antohakiselev@yandex.ru>
2017-08-08 20:22:41 +0500

    Добавлено сравнение списков для методов работы с группами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -5,10 +5,55 @@ public class GroupData {
   private final String header;
   private final String footer;
 
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private  int  id;
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
+  }
+
+  public int  getId() {
+    return   id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = name != null ? name.hashCode() : 0;
+    result = 31 * result + id;
+    return result;
+  }
+
   public GroupData(String name, String header, String footer) {
     this.name = name;
     this.header = header;
     this.footer = footer;
+    this.id=0;
+
+  }
+
+  public GroupData( int id, String name, String header, String footer) {
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+    this.id = id;
   }
 
   public String getName() {
@@ -22,4 +67,6 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
+
 }
+
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -18,7 +19,9 @@ public class GroupCreationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before=app.getGroupHelper().getGroupList();
 
-    app.getGroupHelper().createGroup(new GroupData("test1",null,null));
+
+    GroupData group=new GroupData("test1",null,null);
+    app.getGroupHelper().createGroup(group);
 
 
     List<GroupData> after=app.getGroupHelper().getGroupList();
@@ -26,6 +29,15 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(),before.size()+1);
 
 
+     int max=0;
+     for (GroupData g:after){
+       if (g.getId()>max){
+         max=g.getId();
+       }
+     }
+     group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
 
