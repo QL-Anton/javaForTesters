4cfd6ad7dfd8adaf670b103ffe0000897b32b7a8
QL-Anton <antohakiselev@yandex.ru>
2017-08-26 19:52:23 +0500

    Тест для создания контактов параметризован
    Реализован для теста создания  контактов провайдер тестовых данных, который читают данные из файла в формате XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -2,31 +2,51 @@ package ru.stqa.pft.addressbook.tests;
 
 
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ContactCreationTests extends  TestBase {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list=new ArrayList<Object[]>();
+    BufferedReader reader=new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml="";
+    String line= reader.readLine();
+    while (line!=null){
+      xml +=line;
+      line= reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts= (List<ContactData>)xstream.fromXML(xml);
+    return  contacts.stream().map((g)->new Object[] {g}).collect(Collectors.toList()).iterator();
 
+  }
 
-  @Test(enabled = true)
-  public void testContactCreation() {
+  @Test(enabled = true, dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
 
     app.goTo().HomePage();
     Contacts before = app.contact().all();
-    File photo=new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().
-            withLast_name("Ivanov").
-            withFirst_name("Ivan").
-            withPhoto(photo).
-            withGroup("test2");
+
     app.contact().create(contact);
     app.goTo().HomePage();
     Contacts after = app.contact().all();
