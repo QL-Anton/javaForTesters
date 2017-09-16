4aee81f4e9ab6cefcb341afbfbda658672036565
QL-Anton <antohakiselev@yandex.ru>
2017-09-17 00:03:25 +0500

    Тест логин реализован для версии Мантис 2.5.1

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -11,7 +11,7 @@ public class RegistrationTests extends TestBase {
 
   @Test
   public  void testRegistration(){
-    app.registration().start("user1","user1@mail.ru");
+    app.registration().start("user1", "user1@localhost.localdomain");
 
   }
 }
