c2b1e42eba180426a9024ad9090bd0b23833c77f
QL-Anton <antohakiselev@yandex.ru>
2017-09-17 02:56:16 +0500

    Добавлен тест для регистрации пользователей, который падает

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -14,11 +14,19 @@ public class RegistrationHelper extends  HelperBase {
   }
 
   public void start(String username, String email) {
-    wd.get(app.getProperty("Web.baseUrl") + "/login.php");
+    wd.get(app.getProperty("Web.baseUrl") + "/signup_page.php");
    type(By.name("username"),username);
    type(By.name("email"),email);
    click(By.cssSelector("input[value='Зарегистрироваться']"));
 
+
+  }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"),password);
+    click (By.cssSelector("button[type='submit']"));
 
   }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,17 +1,52 @@
 package ru.stqa.pft.mantis.tests;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Антон on 09.09.2017.
  */
 public class RegistrationTests extends TestBase {
 
-
+@BeforeMethod
+public void startMailServer(){
+  app.mail().start();
+}
 
   @Test
-  public  void testRegistration(){
-    app.registration().start("user1", "user1@localhost.localdomain");
+  public  void testRegistration() throws IOException, MessagingException {
+    String password="password";
+    String user="user1";
+    String email = "user1@localhost.localdomain";
+    app.registration().start(user, email);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    String confirmationLink = findConfirmationLink ( mailMessages, email );
 
+    app.registration ().finish (confirmationLink, password );
+       Assert.assertTrue (app.newSession ().login (  user, password));
+
+
+
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream ().filter ( (m) -> m.to.equals ( email ) ).findFirst ().get ();
+       VerbalExpression regex = VerbalExpression.regex ().find ( "http://" ).nonSpace ().oneOrMore ().build ();
+        regex.getText ( mailMessage.text );
+        return regex.getText ( mailMessage.text );
+  }
+
+
+  @AfterMethod(alwaysRun =true)
+  public void stopMailServer(){
+    app.mail().stop();
   }
 }
